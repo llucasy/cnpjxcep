ce9ded2d91fe2c81027ff91d94c7bd541fc618e7
llucasy <llucasy@hotmail.com>
2021-11-12 03:27:11 -0300

    teste

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 import cheerio from "cheerio";
-import express, { response } from "express";
+import express from "express";
 
 const app = express()
+
+import {Server, Socket} from 'socket.io'
+import { createServer } from 'http';
+
+var server = createServer(app);
+var io = new Server(server)
+
+
 app.set('view engine', 'ejs')
 app.set('views', './views')
 app.use(express.static('public'))
@@ -11,77 +19,95 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+let oldCEP = ''
+let filteredData = []
+let data = []
+
+
 app.get('/cnpjxcep', async(req, res) => {
   const inicial = req.query.inicial
   const final = req.query.final
-
-  let oldCEP = ''
-  let filteredData = []
-  let data = []
   let CEP = inicial
+  
+  io.on('connection', async(socket) => {
 
-  const f1 = async CEP => {
-    await axios(`https://www.google.com.br/search?q=${CEP}+site%3Acnpj.biz`).then(response => {
-      const html = response.data
-      const $ = cheerio.load(html)
-      $('h3', html).each(function() {
-        const title = $(this).text()
-        const link = $(this).parent().attr('href').substring(7, 38)
-        const cnpj = link.substring(17) || ''
-        if(link.substring(8, 12) === 'cnpj') {
-          data.push({title, link, cnpj})
-        }
-      })
-    })
-  }
-
-  const f2 = async (i, jump = 0) => {
-    await axios(`https://cnpj.biz/${data[i + jump].cnpj}`).then( async response => {
-      const html = response.data
-      const $ = cheerio.load(html)
-      const arr = []
-      $('.row p', html).each(async function() {
-        arr.push($(this).text())
-      })
-      data[i + jump].situacao = arr.filter(value => value.substring(0, 9) === 'Situação:')[0].substring(10)
-      data[i + jump].razao = arr.filter(value => value.substring(0, 13) === 'Razão Social:')[0].substring(14)
-    })
-    if ((i + jump) !== data.length - 1) {
-      i++;
-      await f2(i, jump)
-    }
-  }
-
-  const newLoop = async final => {
-    final = Number(final.substring(6))
-    oldCEP = CEP
-    CEP = `${CEP.substring(0,5)}-${Number(CEP.substring(6)) < final? Number(CEP.substring(6)) + 1: Number(CEP.substring(6))}`
-    if (oldCEP === CEP) {
-      return
-    }
-    let jump = data.length - 1
+    socket.emit('info', 'buscando...')
+    
+    data = []
     await f1(CEP)
-    await f2(0, jump)
-    if(data.filter(value => value.situacao === 'Ativa').length < 8) {
-      setTimeout(async() => await newLoop(req.query.final), 2000)
+    await f2(0, 0, socket)
+    filteredData = data.filter(value => value.situacao === 'Ativa')
+  
+    if (filteredData.length < 8) {
+      filteredData = await newLoop(final, inicial, socket)
     } else {
-      console.log(data.filter(value => value.situacao === 'Ativa'))
-      res.render('cnpjxcep', {inicial, final, filteredData: data.filter(value => value.situacao === 'Ativa')})
+      console.log(filteredData)
+      socket.emit('busca', filteredData)
     }
-  }
 
-  await f1(CEP)
-  await f2(0)
-  filteredData = data.filter(value => value.situacao === 'Ativa')
+  })
 
-  if (filteredData.length < 8) {
-    filteredData = await newLoop(req.query.final)
-  } else {
-    console.log(filteredData)
-  }
+  res.render('cnpjxcep', {inicial, final})
 
 })
 
+const f1 = async CEP => {
+  await axios(`https://www.google.com.br/search?q=${CEP}+site%3Acnpj.biz`).then(response => {
+    const html = response.data
+    const $ = cheerio.load(html)
+    $('h3', html).each(function() {
+      const title = $(this).text()
+      const link = $(this).parent().attr('href').substring(7, 38)
+      const cnpj = link.substring(17) || ''
+      if(link.substring(8, 12) === 'cnpj') {
+        data.push({title, link, cnpj})
+      }
+    })
+  })
+}
+
+const f2 = async (i, jump = 0, socket) => {
+  await axios(`https://cnpj.biz/${data[i + jump].cnpj}`).then( async response => {
+    const html = response.data
+    const $ = cheerio.load(html)
+    const arr = []
+    $('.row p', html).each(async function() {
+      arr.push($(this).text())
+    })
+    data[i + jump].situacao = arr.filter(value => value.substring(0, 9) === 'Situação:')[0].substring(10)
+    data[i + jump].razao = arr.filter(value => value.substring(0, 13) === 'Razão Social:')[0].substring(14)
+  })
+  if(data[i + jump].situacao = 'Ativa') {
+    socket.emit('busca', data[i + jump])
+  }
+  if ((i + jump) !== data.length - 1) {
+    i++;
+    await f2(i, jump, socket)
+  }
+}
+
+const newLoop = async (final, inicial, socket) => {
+  final = Number(final.substring(6))
+  oldCEP = inicial
+  inicial = `${inicial.substring(0,5)}-${Number(inicial.substring(6)) < final? Number(inicial.substring(6)) + 1: Number(inicial.substring(6))}`
+  if (oldCEP === inicial) {
+    return
+  }
+  let jump = data.length - 1
+  await f1(inicial)
+  await f2(0, jump, socket)
+  if(data.filter(value => value.situacao === 'Ativa').length < 8) {
+    setTimeout(async() => await newLoop(final, inicial), 2000)
+  } else {
+    socket.emit('busca', data.filter(value => value.situacao === 'Ativa'))
+    console.log(data.filter(value => value.situacao === 'Ativa'))
+  }
+}
+
+
+
+
+
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('servidor no ar'))
+server.listen(port, () => console.log('servidor no ar'))
